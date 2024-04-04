8e1fead74ca6827cb009562435b7a5471a4554eb
Nelson Nobre <technobre@gmail.com>
2024-04-04 01:43:36 +0100

    test

--- misc/generate-pdf.js
+++ misc/generate-pdf.js
@@ -7,6 +7,13 @@ const path = require('path');
 
     const indexPath = path.resolve(__dirname, './src/index.html');
 
+    try {
+        require.resolve(indexPath);
+    } catch (error) {
+        console.error(`The file ${indexPath} does not exist. Please run 'npm run build' first.`);
+        process.exit(1);
+    }
+
     await page.goto(`file://${indexPath}`, {waitUntil: 'networkidle2'});
 
     await page.pdf({path: 'resume.pdf', format: 'A4'});
